a8d63f8a0c4e0024236b4bda293dc57a132dbe62
xiezeyu <742855476@qq.com>
2019-11-28 14:09:12 +0800

    修改了内容

--- git-test/src/itheima/FirstClass.java
+++ git-test/src/itheima/FirstClass.java
@@ -4,4 +4,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("haha");
     }
+    public void test(){
+        int i = 10;
+        System.out.println(i);
+    }
 }
