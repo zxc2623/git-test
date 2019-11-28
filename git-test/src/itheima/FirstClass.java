ca20707d2821445aed0da50f85c12076f6260adf
xiezeyu <742855476@qq.com>
2019-11-28 14:36:22 +0800

    修改了些内容

--- git-test/src/itheima/FirstClass.java
+++ git-test/src/itheima/FirstClass.java
@@ -3,9 +3,11 @@ package itheima;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("haha");
+        //hello test
     }
     public void test(){
         int i = 10;
+        i+=1;
         System.out.println(i);
     }
 }
